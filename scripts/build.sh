1866d03b9e132a3bc3cfc98fb61363800a6cb174
Brett Dutton <brett@b2bconsultancy.asia>
2025-01-03 14:11:14 +0700

    Created build script

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go build -o mydecrypt cmd/mydecrypt/mydecrypt.go                                                                                                   (main)
+go build -o myencrypt cmd/myencrypt/myencrypt.go
